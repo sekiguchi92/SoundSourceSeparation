d8b727cd26b3b20aded6b0a296b3a738caa42cee
sekiguchi92 <sekiguchi92@gmail.com>
2021-12-13 10:29:32 +0900

    Fixed a bug

--- Jointly_Diagonalizable_FullRank_Model/FastFCA.py
+++ Jointly_Diagonalizable_FullRank_Model/FastFCA.py
@@ -11,7 +11,7 @@ import pickle as pic
 from configure_FastModel import *
 
 
-class FastFCA():
+class FastFCA:
     """ Blind Source Separation Using Fast Full-rank Covariance Analysis (FastFCA)
 
     X_FTM: the observed complex spectrogram
@@ -59,13 +59,11 @@ class FastFCA():
         else:
             raise ValueError("n_bit must be 32 or 64")
 
-
     def convert_to_NumpyArray(self, data):
         if self.xp == np:
             return data
         else:
-            return cuda.to_cpu(data)
-
+            return self.xp.asnumpy(data)
 
     def load_spectrogram(self, X_FTM):
         """ load complex spectrogram
@@ -79,11 +77,9 @@ class FastFCA():
         self.X_FTM = self.xp.asarray(X_FTM, dtype=self.TYPE_COMPLEX)
         self.XX_FTMM = self.X_FTM[:, :, :, None] @ self.X_FTM[:, :, None, :].conj()
 
-
     def initialize_PSD(self):
         self.lambda_NFT = self.xp.random.random([self.n_source, self.n_freq, self.n_time]).astype(self.TYPE_FLOAT)
         self.lambda_NFT[0] = self.xp.abs(self.X_FTM.mean(axis=2)) ** 2
-
 
     def initialize_covarianceMatrix(self):
         if ("unit" in self.init_SCM) or ("circular" in self.init_SCM):
@@ -92,7 +88,9 @@ class FastFCA():
             for m in range(self.n_mic):
                 self.G_NFM[m % self.n_source, :, m] = 1
         elif "obs" in self.init_SCM:
-            mixture_covarianceMatrix_FMM = self.XX_FTMM.sum(axis=1) / (self.xp.trace(self.XX_FTMM, axis1=2, axis2=3).sum(axis=1))[:, None, None]
+            mixture_covarianceMatrix_FMM = (
+                self.XX_FTMM.sum(axis=1) / (self.xp.trace(self.XX_FTMM, axis1=2, axis2=3).sum(axis=1))[:, None, None]
+            )
             eig_val, eig_vec = np.linalg.eigh(self.convert_to_NumpyArray(mixture_covarianceMatrix_FMM))
             eig_val, eig_vec = eig_val[:, ::-1], eig_vec[:, :, ::-1]
             self.Q_FMM = self.xp.asarray(eig_vec).transpose(0, 2, 1).conj()
@@ -100,6 +98,7 @@ class FastFCA():
             self.G_NFM[0, :, 0] = 1
         elif "gradual" in self.init_SCM:
             from FastMNMF import FastMNMF
+
             fastmnmf1 = FastMNMF(n_source=self.n_source, n_basis=2, init_SCM="circular", xp=self.xp, n_bit=self.n_bit)
             fastmnmf1.load_spectrogram(self.X_FTM)
             fastmnmf1.solve(n_iteration=30)
@@ -108,9 +107,12 @@ class FastFCA():
         elif "ILRMA" in self.init_SCM:
             sys.path.append("../Rank1_Model")
             from ILRMA import ILRMA
+
             ilrma = ILRMA(n_basis=2, init_SCM="unit", xp=self.xp, n_bit=self.n_bit)
             ilrma.load_spectrogram(self.X_FTM)
-            ilrma.solve(n_iteration=15, save_likelihood=False, save_wav=False, save_path="./", interval_save_parameter=1000)
+            ilrma.solve(
+                n_iteration=15, save_likelihood=False, save_wav=False, save_path="./", interval_save_parameter=1000
+            )
             separated_spec_power = self.xp.abs(ilrma.separated_spec).mean(axis=(1, 2))
             self.Q_FMM = ilrma.SeparationMatrix_FMM
             self.G_NFM = self.xp.ones([self.n_source, self.n_freq, self.n_mic], dtype=self.TYPE_FLOAT) * 1e-2
@@ -122,14 +124,12 @@ class FastFCA():
 
         self.normalize()
 
-
     def reset_variable(self):
         if self.xp == np:
             self.Qx_power_FTM = self.xp.abs((self.Q_FMM[:, None] @ self.X_FTM[:, :, :, None])[:, :, :, 0]) ** 2
         else:
             self.Qx_power_FTM = self.xp.abs((self.Q_FMM[:, None] * self.X_FTM[:, :, None]).sum(axis=3)) ** 2
         self.Y_FTM = (self.lambda_NFT[..., None] * self.G_NFM[:, :, None]).sum(axis=0)
-
 
     def make_filename_suffix(self):
         self.filename_suffix = f"S={self.n_source}-it={self.n_iteration}-init={self.init_SCM}"
@@ -140,8 +140,16 @@ class FastFCA():
             self.filename_suffix += f"-ID={self.file_id}"
         print("param:", self.filename_suffix)
 
-
-    def solve(self, n_iteration=100, save_likelihood=False, save_parameter=False, save_wav=False, save_path="./", interval_save_parameter=30, mic_index=MIC_INDEX):
+    def solve(
+        self,
+        n_iteration=100,
+        save_likelihood=False,
+        save_parameter=False,
+        save_wav=False,
+        save_path="./",
+        interval_save_parameter=30,
+        mic_index=MIC_INDEX,
+    ):
         """
         Parameters:
             save_likelihood: boolean
@@ -165,26 +173,38 @@ class FastFCA():
         for it in tqdm(range(self.n_iteration)):
             self.update()
 
-            if save_parameter and ((it+1) % interval_save_parameter == 0) and ((it+1) != self.n_iteration):
-                self.save_parameter(save_path+"{}-parameter-{}-{}.pic".format(self.method_name, self.filename_suffix, it + 1))
+            if save_parameter and ((it + 1) % interval_save_parameter == 0) and ((it + 1) != self.n_iteration):
+                self.save_parameter(
+                    save_path + "{}-parameter-{}-{}.pic".format(self.method_name, self.filename_suffix, it + 1)
+                )
 
-            if save_wav and ((it+1) % interval_save_parameter == 0) and ((it+1) != self.n_iteration):
+            if save_wav and ((it + 1) % interval_save_parameter == 0) and ((it + 1) != self.n_iteration):
                 self.separate_FastWienerFilter(mic_index=MIC_INDEX)
-                self.save_separated_signal(save_path+"{}-sep-Wiener-{}-{}.wav".format(self.method_name, self.filename_suffix, it + 1))
+                self.save_separated_signal(
+                    save_path + "{}-sep-Wiener-{}-{}.wav".format(self.method_name, self.filename_suffix, it + 1)
+                )
 
-            if save_likelihood and ((it+1) % interval_save_parameter == 0) and ((it+1) != self.n_iteration):
+            if save_likelihood and ((it + 1) % interval_save_parameter == 0) and ((it + 1) != self.n_iteration):
                 log_likelihood_array.append(self.calculate_log_likelihood())
 
         if save_parameter:
-            self.save_parameter(save_path+"{}-parameter-{}.pic".format(self.method_name, self.filename_suffix))
+            self.save_parameter(save_path + "{}-parameter-{}.pic".format(self.method_name, self.filename_suffix))
 
         if save_likelihood:
             log_likelihood_array.append(self.calculate_log_likelihood())
-            pic.dump(log_likelihood_array, open(save_path + "{}-likelihood-interval={}-{}.pic".format(self.method_name, interval_save_parameter, self.filename_suffix), "wb"))
+            pic.dump(
+                log_likelihood_array,
+                open(
+                    save_path
+                    + "{}-likelihood-interval={}-{}.pic".format(
+                        self.method_name, interval_save_parameter, self.filename_suffix
+                    ),
+                    "wb",
+                ),
+            )
 
         self.separate_FastWienerFilter(mic_index=mic_index)
-        self.save_separated_signal(save_path+"{}-sep-Wiener-{}.wav".format(self.method_name, self.filename_suffix))
-
+        self.save_separated_signal(save_path + "{}-sep-Wiener-{}.wav".format(self.method_name, self.filename_suffix))
 
     def update(self):
         self.update_lambda()
@@ -192,29 +212,27 @@ class FastFCA():
         self.udpate_Diagonalizer()
         self.normalize()
 
-
     def udpate_Diagonalizer(self):
         for m in range(self.n_mic):
             V_FMM = (self.XX_FTMM / self.Y_FTM[:, :, m, None, None]).mean(axis=1)
             tmp_FM = self.xp.linalg.inv(self.Q_FMM @ V_FMM)[:, :, m]
-            self.Q_FMM[:, m] = (tmp_FM / self.xp.sqrt(( (tmp_FM.conj()[:, :, None] * V_FMM).sum(axis=1) * tmp_FM).sum(axis=1) )[:, None]).conj()
-
+            self.Q_FMM[:, m] = (
+                tmp_FM / self.xp.sqrt(((tmp_FM.conj()[:, :, None] * V_FMM).sum(axis=1) * tmp_FM).sum(axis=1))[:, None]
+            ).conj()
 
     def update_CovarianceDiagElement(self):
-        a_1 = (self.lambda_NFT[..., None] * (self.Qx_power_FTM / (self.Y_FTM ** 2))[None]).sum(axis=2) # N F T M
+        a_1 = (self.lambda_NFT[..., None] * (self.Qx_power_FTM / (self.Y_FTM ** 2))[None]).sum(axis=2)  # N F T M
         b_1 = (self.lambda_NFT[..., None] / self.Y_FTM[None]).sum(axis=2)
         self.G_NFM *= self.xp.sqrt(a_1 / b_1)
         self.G_NFM += EPS
         self.Y_FTM = (self.lambda_NFT[..., None] * self.G_NFM[:, :, None]).sum(axis=0)
 
-
     def update_lambda(self):
-        a = (self.G_NFM[:, :, None] * (self.Qx_power_FTM / (self.Y_FTM ** 2))[None]).sum(axis=3) # N F T
+        a = (self.G_NFM[:, :, None] * (self.Qx_power_FTM / (self.Y_FTM ** 2))[None]).sum(axis=3)  # N F T
         b = (self.G_NFM[:, :, None] / self.Y_FTM[None]).sum(axis=3)
-        self.lambda_NFT *= self.xp.sqrt(a / b) 
+        self.lambda_NFT *= self.xp.sqrt(a / b)
         self.lambda_NFT += EPS
         self.Y_FTM = (self.lambda_NFT[..., None] * self.G_NFM[:, :, None]).sum(axis=0)
-
 
     def normalize(self):
         phi_F = self.xp.sum(self.Q_FMM * self.Q_FMM.conj(), axis=(1, 2)).real / self.n_mic
@@ -227,32 +245,61 @@ class FastFCA():
 
         self.reset_variable()
 
-
     def calculate_log_likelihood(self):
-        return (-(self.Qx_power_FTM / self.Y_FTM).sum() + self.n_time * np.log(np.linalg.det(self.convert_to_NumpyArray(self.Q_FMM @ self.Q_FMM.conj().transpose(0, 2, 1) ) ) ).sum() - self.xp.log(self.Y_FTM).sum()).real - self.n_mic * self.n_freq * self.n_time * np.log(np.pi)
-
+        return (
+            -(self.Qx_power_FTM / self.Y_FTM).sum()
+            + self.n_time
+            * np.log(
+                np.linalg.det(self.convert_to_NumpyArray(self.Q_FMM @ self.Q_FMM.conj().transpose(0, 2, 1)))
+            ).sum()
+            - self.xp.log(self.Y_FTM).sum()
+        ).real - self.n_mic * self.n_freq * self.n_time * np.log(np.pi)
 
     def calculate_covarianceMatrix(self):
-        covarianceMatrix_NFMM = self.xp.zeros([self.n_source, self.n_freq, self.n_mic, self.n_mic], dtype=self.TYPE_COMPLEX)
+        covarianceMatrix_NFMM = self.xp.zeros(
+            [self.n_source, self.n_freq, self.n_mic, self.n_mic], dtype=self.TYPE_COMPLEX
+        )
         diagonalizer_inv_FMM = self.xp.linalg.inv(self.Q_FMM)
         for n in range(self.n_source):
             for f in range(self.n_freq):
-                covarianceMatrix_NFMM[n, f] = diagonalizer_inv_FMM[f] @ np.diag(self.G_NFM[n, f]) @ diagonalizer_inv_FMM[f].conj().T
+                covarianceMatrix_NFMM[n, f] = (
+                    diagonalizer_inv_FMM[f] @ np.diag(self.G_NFM[n, f]) @ diagonalizer_inv_FMM[f].conj().T
+                )
         return covarianceMatrix_NFMM
-
 
     def separate_FastWienerFilter(self, source_index=None, mic_index=MIC_INDEX):
         Qx_FTM = (self.Q_FMM[:, None] * self.X_FTM[:, :, None]).sum(axis=3)
         diagonalizer_inv_FMM = self.xp.linalg.inv(self.Q_FMM)
         if source_index != None:
-            self.separated_spec = self.convert_to_NumpyArray((diagonalizer_inv_FMM[:, None] @ (Qx_FTM * ( (self.lambda_NFT[source_index, :, :, None] * self.G_NFM[source_index, :, None]) / (self.lambda_NFT[..., None]* self.G_NFM[:, :, None]).sum(axis=0) ) )[..., None])[:, :, mic_index, 0])
+            self.separated_spec = self.convert_to_NumpyArray(
+                (
+                    diagonalizer_inv_FMM[:, None]
+                    @ (
+                        Qx_FTM
+                        * (
+                            (self.lambda_NFT[source_index, :, :, None] * self.G_NFM[source_index, :, None])
+                            / (self.lambda_NFT[..., None] * self.G_NFM[:, :, None]).sum(axis=0)
+                        )
+                    )[..., None]
+                )[:, :, mic_index, 0]
+            )
         else:
             for n in range(self.n_source):
-                tmp = self.convert_to_NumpyArray((diagonalizer_inv_FMM[:, None] @ (Qx_FTM * ( (self.lambda_NFT[n, :, :, None] * self.G_NFM[n, :, None]) / (self.lambda_NFT[..., None]* self.G_NFM[:, :, None]).sum(axis=0) ) )[..., None])[:, :, mic_index, 0])
+                tmp = self.convert_to_NumpyArray(
+                    (
+                        diagonalizer_inv_FMM[:, None]
+                        @ (
+                            Qx_FTM
+                            * (
+                                (self.lambda_NFT[n, :, :, None] * self.G_NFM[n, :, None])
+                                / (self.lambda_NFT[..., None] * self.G_NFM[:, :, None]).sum(axis=0)
+                            )
+                        )[..., None]
+                    )[:, :, mic_index, 0]
+                )
                 if n == 0:
                     self.separated_spec = np.zeros([self.n_source, tmp.shape[0], tmp.shape[1]], dtype=np.complex)
                 self.separated_spec[n] = tmp
-
 
     def save_separated_signal(self, save_filename="sample.wav"):
         self.separated_spec = self.convert_to_NumpyArray(self.separated_spec)
@@ -270,13 +317,11 @@ class FastFCA():
             separated_signal /= np.abs(separated_signal).max() * 1.2
             sf.write(save_filename, separated_signal.T, 16000)
 
-
     def save_parameter(self, filename):
         param_list = [self.lambda_NFT, self.G_NFM, self.Q_FMM]
         if self.xp != np:
             param_list = [self.convert_to_NumpyArray(param) for param in param_list]
         pic.dump(param_list, open(filename, "wb"))
-
 
     def load_parameter(self, filename):
         param_list = pic.load(open(filename, "rb"))
@@ -293,21 +338,22 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_filename', type= str, help='filename of the multichannel observed signals')
-    parser.add_argument(     '--file_id', type= str, default=    "None", help='file id')
-    parser.add_argument(         '--gpu', type= int, default=         0, help='GPU ID')
-    parser.add_argument(       '--n_fft', type= int, default=      1024, help='number of frequencies')
-    parser.add_argument(    '--n_source', type= int, default=         2, help='number of noise')
-    parser.add_argument(    '--init_SCM', type= str, default= "gradual", help='circular, gradual, obs, ILRMA')
-    parser.add_argument( '--n_iteration', type= int, default=       100, help='number of iteration')
-    parser.add_argument(       '--n_mic', type= int, default=         8, help='number of microphones')
-    parser.add_argument(       '--n_bit', type= int, default=        64, help='number of bits for floating point number')
+    parser.add_argument("input_filename", type=str, help="filename of the multichannel observed signals")
+    parser.add_argument("--file_id", type=str, default="None", help="file id")
+    parser.add_argument("--gpu", type=int, default=0, help="GPU ID")
+    parser.add_argument("--n_fft", type=int, default=1024, help="number of frequencies")
+    parser.add_argument("--n_source", type=int, default=2, help="number of noise")
+    parser.add_argument("--init_SCM", type=str, default="gradual", help="circular, gradual, obs, ILRMA")
+    parser.add_argument("--n_iteration", type=int, default=100, help="number of iteration")
+    parser.add_argument("--n_mic", type=int, default=8, help="number of microphones")
+    parser.add_argument("--n_bit", type=int, default=64, help="number of bits for floating point number")
     args = parser.parse_args()
 
     if args.gpu < 0:
         import numpy as xp
     else:
         import cupy as xp
+
         print("Use GPU " + str(args.gpu))
         xp.cuda.Device(args.gpu).use()
 
@@ -315,7 +361,7 @@ if __name__ == "__main__":
     wav = wav.T
     M = min(args.n_mic, len(wav))
     for m in range(M):
-        tmp = librosa.core.stft(wav[m], n_fft=args.n_fft, hop_length=int(args.n_fft/4))
+        tmp = librosa.core.stft(wav[m], n_fft=args.n_fft, hop_length=int(args.n_fft / 4))
         if m == 0:
             spec = np.zeros([tmp.shape[0], tmp.shape[1], M], dtype=np.complex)
         spec[:, :, m] = tmp
@@ -323,4 +369,12 @@ if __name__ == "__main__":
     separater = FastFCA(n_source=args.n_source, xp=xp, init_SCM=args.init_SCM, n_bit=args.n_bit, seed=0)
     separater.load_spectrogram(spec)
     separater.file_id = args.file_id
-    separater.solve(n_iteration=args.n_iteration, save_likelihood=False, save_parameter=False, save_wav=False, save_path="./", interval_save_parameter=25)
+    separater.solve(
+        n_iteration=args.n_iteration,
+        save_likelihood=False,
+        save_parameter=False,
+        save_wav=False,
+        save_path="./",
+        interval_save_parameter=25,
+    )
+
